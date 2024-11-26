cf7522e8a415b1be39d392e7b8140b985bd8615e
USERLI <hliek@connect.ust.hk>
2024-11-26 20:20:49 +0800

    Your commit message here

--- gem_fire.js
+++ gem_fire.js
@@ -1,15 +1,18 @@
-const Fire = function(ctx, x, y) {
+const Fire = function(ctx, x, y, firetype){
+    const sequences = {
+        realfire:  { x: 0, y:  160, width: 16, height: 16, count: 6, timing: 200, loop: true },
+        boom    :  { x: 64, y:  112, width: 16, height: 16, count: 6, timing: 200, loop: true }
+    };
     const sprite = Sprite(ctx, x, y);
-    sprite.setSequence({ x: 0, y: 160, width: 16, height: 16, count: 8, timing: 200, loop: true }) // Use your fire sprite sheet
-          .setScale(2)
-          .useSheet("object_sprites.png"); 
-
-    const draw = function() {
-        sprite.draw();
-    };
-
+    sprite.setSequence(sequences[firetype])
+    .setScale(2)
+    .setShadowScale({ x: 0.75, y: 0.2 })
+    .useSheet("object_sprites.png");
     return {
-        draw: draw,
-        getBoundingBox: sprite.getBoundingBox
+        getXY: sprite.getXY,
+        setXY: sprite.setXY,
+        getBoundingBox: sprite.getBoundingBox,
+        draw: sprite.draw,
+        update: sprite.update
     };
-};
+}
