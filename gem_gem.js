a8bb721b66bde5c4912af4976a322e2e0479911a
CYRL1234 <ryanlieu08@gmail.com>
2024-11-26 19:21:27 +0800

    Update gem_gem.js

--- gem_gem.js
+++ gem_gem.js
@@ -4,4 +4,24 @@ const Gem = function(ctx, x, y) {
           .setScale(2)
           .useSheet("object_sprites.png"); // Use your gem sprite sheet
 
-    const draw =
+    let collected = false;
+
+    const draw = function() {
+        if (!collected) {
+            sprite.draw();
+        }
+    };
+
+    const collect = function() {
+        collected = true; // Mark the gem as collected
+    };
+
+    return {
+        getXY: sprite.getXY,
+        draw: draw,
+        collect: collect,
+        isCollected: () => collected,
+        setXY: sprite.setXY,
+        getBoundingBox: sprite.getBoundingBox
+    };
+};
